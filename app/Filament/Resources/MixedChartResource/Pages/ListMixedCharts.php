d178e764afd6a9e1227212a1db20c640bef33fa8
marco76tv <marco.sottana@gmail.com>
2026-02-24 14:05:33 +0100

    .

--- app/Filament/Resources/MixedChartResource/Pages/ListMixedCharts.php
+++ app/Filament/Resources/MixedChartResource/Pages/ListMixedCharts.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Chart\Filament\Resources\MixedChartResource\Pages;
 
+use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Modules\Chart\Filament\Resources\MixedChartResource;
 use Modules\UI\Enums\TableLayoutEnum;
