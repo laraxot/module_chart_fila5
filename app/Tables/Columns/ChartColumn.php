68d737fd7217850d671aaa2fc5d735d951eea252
marco76tv <marco.sottana@gmail.com>
2026-03-09 09:13:14 +0100

    .

--- app/Tables/Columns/ChartColumn.php
+++ app/Tables/Columns/ChartColumn.php
@@ -10,18 +10,10 @@ use Modules\Chart\Datas\AnswersChartData;
 
 use function Safe\json_encode;
 
-// use Illuminate\Session\SessionManager;
-
 class ChartColumn extends Column
 {
-    // class ChartColumn extends Component
-    public string $dataChecksum;
-
+    public string $dataChecksum = '';
     public ?string $filter = null;
-
-    // protected string $view='filament::widgets.chart-widget';
-    // protected $listeners = ['refreshChartColumn' => '$refresh'];
-    /** @var array<string, mixed> */
     public array $chartData = [
         'datasets' => [
             [
@@ -31,127 +23,41 @@ class ChartColumn extends Column
         ],
         'labels' => [],
     ];
-
     public string $chartType = 'bar';
-
-    /** @var array<string, mixed> */
     public array $chartOptions = [];
-
-    protected static ?string $heading = null;
-
-    protected static ?string $maxHeight = null;
-
-    /** @var array<string, mixed>|null */
-    protected static ?array $options = null;
-
-    /** @var array<string, mixed>|null */
     protected ?array $cachedData = null;
-
     protected string $view = 'chart::tables.columns.chart-column';
 
     public function applyAnswersChartData(AnswersChartData $answersChartData): self
     {
-        $chartData = $answersChartData->getChartJsData();
-        $chartType = $answersChartData->getChartJsType();
-        $chartOptions = $answersChartData->getChartJsOptions();
-        $cachedData = null;
-
-        // dddx([$this->getCachedData());
-        // $this->emit('refreshChartColumn');
-        // filterChartData
+        $this->chartData = $answersChartData->getChartJsData();
+        $this->chartType = $answersChartData->getChartJsType();
+        $this->chartOptions = $answersChartData->getChartJsOptionsArray();
         return $this;
     }
 
-    public function render(): View
-    {
-        $viewParams = [
-            'obj' => $this,
-        ];
-
-        /** @var view-string $viewName */
-        $viewName = $view;
-
-        return view($viewName, $viewParams);
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
     public function getCachedData(): array
     {
-        return $cachedData ??= $this->getData();
+        return $this->cachedData ??= $this->getData();
     }
 
-    public function getMaxHeight(): ?string
-    {
-        return static::$maxHeight;
-    }
-
-    /**
-     * @return array<string, mixed>|null
-     */
     public function getOptions(): ?array
     {
-        return $chartOptions;
+        return $this->chartOptions;
     }
 
     public function getType(): string
     {
-        return $chartType;
-    }
-
-    public function updateChartData(): void
-    {
-        $newDataChecksum = $this->generateDataChecksum();
-
-        if ($newDataChecksum !== $dataChecksum)
-            $dataChecksum = $newDataChecksum;
-
-            // Assert::methodNotExists($this, 'emitSelf', $message = 'function emitSelf not exists');
-            // NON E' LIVEWIRE
-            // $this->emitSelf('updateChartData', [)
-            //    'data' => $this->getCachedData()
-            // ]);
-        }
-    }
-
-    public function updatedFilter(): void
-    {
-        $newDataChecksum = $this->generateDataChecksum();
-
-        if ($newDataChecksum !== $dataChecksum)
-            $dataChecksum = $newDataChecksum;
-
-            // NON E' LIVEWIRE
-            // $this->emitSelf('updateChartData', [)
-            //    'data' => $this->getCachedData()
-            // ]);
-        }
-    }
-
-    public function getHeading(): ?string
-    {
-        return static::$heading;
+        return $this->chartType;
     }
 
     protected function generateDataChecksum(): string
     {
-        return md5(json_encode($getCachedData()));
+        return md5(json_encode($this->getCachedData()));
     }
 
-    /**
-     * @return array<string, mixed>
-     */
     protected function getData(): array
     {
-        return $chartData;
-    }
-
-    /**
-     * @return array<string, mixed>|null
-     */
-    protected function getFilters(): ?array
-    {
-        return null;
+        return $this->chartData;
     }
 }
