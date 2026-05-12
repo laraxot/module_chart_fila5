4a1f29f1d2c2cebd1824c47c364b7f660d000663
marco76tv <marco.sottana@gmail.com>
2026-05-12 08:10:02 +0200

    .

--- app/Filament/Resources/MixedChartResource/Schemas/MixedChartForm.php
+++ app/Filament/Resources/MixedChartResource/Schemas/MixedChartForm.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\base_quaeris_fila5\var\www\_bases\base_quaeris_fila5\laravel\Modules\Chart\app\Filament\Resources\MixedChartResource\Schemas;
+namespace Modules\Chart\app\Filament\Resources\MixedChartResource\Schemas;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
