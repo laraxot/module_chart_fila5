eab7407c05be050432b79dceccce3a61169b4f1c
marco76tv <marco.sottana@gmail.com>
2026-07-20 19:26:01 +0200

    .

---
+++ lang/it/mixed_charts.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'id' => ['label' => 'id'],
+        'title' => ['label' => 'title'],
+        'type' => ['label' => 'type'],
+        'created_at' => ['label' => 'created_at'],
+    ],
+];
